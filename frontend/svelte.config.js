d17eba79488c4adfef723f452ae8d8921913ed93
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-07-29 23:40:56 -0400

    ci: fix path trigger deploy

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare-workers';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,6 @@ const config = {
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
-            config: 'wrangler.toml',
             platformProxy: {
                 configPath: 'wrangler.toml',
                 environment: undefined,
