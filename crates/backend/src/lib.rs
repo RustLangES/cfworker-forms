fe581abb956b8954ea05278399617fc17643bb54
Brayan-724 <bjqf0724@gmail.com>
2024-08-18 01:11:05 -0700

    fix(backend): CORS headers
    Signed-off-by: Brayan-724 <bjqf0724@gmail.com>

--- crates/backend/src/lib.rs
+++ crates/backend/src/lib.rs
@@ -6,7 +6,7 @@ mod question;
 mod session;
 mod shared;
 
-use worker::{event, Context, Env, Request, Response, Result, Router};
+use worker::{event, Context, Env, Method, Request, Response, Result, Router};
 
 pub type RouterContext = worker::RouteContext<()>;
 
@@ -21,6 +21,32 @@ fn start() {
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     let mut router = Router::new();
+
+    // Prevent errors from preflight call
+    // - https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+    // - https://developers.cloudflare.com/workers/examples/cors-header-proxy/
+    router = router.options("/*path", |req, _| {
+        let headers = req.headers();
+        if headers.has("Origin")?
+            && headers.has("Access-Control-Request-Method")?
+            && headers.has("Access-Control-Request-Headers")?
+        {
+            // Handle CORS preflight requests.
+            Ok(worker::Response::builder()
+                .with_header("Access-Control-Allow-Origin", "*")?
+                .with_header("Access-Control-Allow-Methods", "GET,HEAD,POST,OPTIONS")?
+                .with_header("Access-Control-Max-Age", "86400")?
+                .with_header(
+                    "Access-Control-Allow-Headers",
+                    &headers.get("Access-Control-Request-Headers")?.unwrap(),
+                )?
+                .empty())
+        } else {
+            Ok(worker::Response::builder()
+                .with_header("Allow", "GET, HEAD, POST, OPTIONS")?
+                .empty())
+        }
+    });
 
     router = router
         // General Login
@@ -57,5 +83,17 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
         .get_async("/api/form/:form_id/question/:id/answer", answer::get)
         .post_async("/api/form/:form_id/question/:id/answer", answer::post);
 
-    router.run(req, env).await
+    let method = req.method();
+    let origin = &req.url()?.origin().ascii_serialization();
+
+    let mut res = router.run(req, env).await?;
+
+    if method != Method::Options {
+        // CORS :\
+        res.headers_mut()
+            .set("Access-Control-Allow-Origin", "*")?;
+        res.headers_mut().set("Vary", "Origin")?;
+    }
+
+    Ok(res)
 }
